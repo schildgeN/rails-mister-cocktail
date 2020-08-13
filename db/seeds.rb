26a63522e04ab1527f4d7e519cc5890bbcd54b1b
Nelson Schildge <nelsonschildge@gmail.com>
2020-08-13 16:34:18 +0200

    front on work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
 
 puts "Cleaning database..."
 Cocktail.destroy_all
-Ingredient.destroy_all
-Dose.destroy_all
 
-puts "Creating Cocktails..."
-long_island = Cocktail.create(name: "Long Island")
-w_coke = Cocktail.create(name: "Whisky Coke")
-sontb = Cocktail.create(name: "Sex On The Beach")
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = open(url).read
+results = JSON.parse(ingredients)
 
 puts "Creating ingredients..."
-i_lemon = Ingredient.create(name: "lemon")
-i_ice = Ingredient.create(name: "ice")
-i_mint = Ingredient.create(name: "mint leaves")
-i_icetea = Ingredient.create(name: "ice tea")
+results["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
 
-puts "Creating doses..."
-Dose.create(description: "50mg", cocktail: long_island, ingredient: i_lemon)
-Dose.create(description: "25cl", cocktail: w_coke, ingredient: i_ice)
-Dose.create(description: "3mg", cocktail: sontb, ingredient: i_mint)
-Dose.create(description: "60cl", cocktail: long_island, ingredient: i_icetea)
+puts "Creating Cocktails..."
+cocktails = ("odkeau, Bloody Mary, Russe blanc, Octobre rouge, Russe noir, Screwdriver, Trou noir, Chien enragé, PPP, Sex on the beach, Blue Lagoon, Cosmopolitan").split(",")
+cocktails.each do |cocktail|
+  Cocktail.create(name: cocktail)
+end
+
+# i_lemon = Ingredient.create(name: "lemon")
+# i_ice = Ingredient.create(name: "ice")
+# i_mint = Ingredient.create(name: "mint leaves")
+# i_icetea = Ingredient.create(name: "ice tea")
+
+# Dose.create(description: "50mg", cocktail: long_island, ingredient: i)
+# Dose.create(description: "25cl", cocktail: w_coke, ingredient: i)
+# Dose.create(description: "3mg", cocktail: sontb, ingredient: i)
+# Dose.create(description: "60cl", cocktail: long_island, ingredient: i)
 puts "Finished!"
