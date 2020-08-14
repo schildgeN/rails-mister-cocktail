378ecf347ecbaa8f657075af09f9f89ede047477
Nelson Schildge <nelsonschildge@gmail.com>
2020-08-14 17:28:06 +0200

    trying to debug heroku fatals

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
+  get "cocktails", to: "cocktails#index", as: '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
